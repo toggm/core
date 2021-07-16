372046a4f18c077434ed15897f66d7d252937598
mike.toggweiler <mike.toggweiler@tegonal.com>
2021-07-16 15:18:24 +0200

    Added support for cover controlled by two coils

--- homeassistant/components/modbus/__init__.py
+++ homeassistant/components/modbus/__init__.py
@@ -59,6 +59,7 @@ from .const import (  # noqa: F401
     CALL_TYPE_WRITE_REGISTER,
     CALL_TYPE_X_COILS,
     CALL_TYPE_X_REGISTER_HOLDINGS,
+    CONF_ADDRESS_CLOSE,
     CONF_BAUDRATE,
     CONF_BYTESIZE,
     CONF_CLIMATES,
@@ -255,12 +256,11 @@ CLIMATE_SCHEMA = vol.All(
 
 COVERS_SCHEMA = BASE_COMPONENT_SCHEMA.extend(
     {
-        vol.Optional(CONF_INPUT_TYPE, default=CALL_TYPE_REGISTER_HOLDING,): vol.In(
-            [
-                CALL_TYPE_REGISTER_HOLDING,
-                CALL_TYPE_COIL,
-            ]
-        ),
+        vol.Optional(CONF_ADDRESS_CLOSE): cv.positive_int,
+        vol.Optional(
+            CONF_INPUT_TYPE,
+            default=CALL_TYPE_REGISTER_HOLDING,
+        ): vol.In([CALL_TYPE_REGISTER_HOLDING, CALL_TYPE_COIL]),
         vol.Optional(CONF_DEVICE_CLASS): COVER_DEVICE_CLASSES_SCHEMA,
         vol.Optional(CONF_STATE_CLOSED, default=0): cv.positive_int,
         vol.Optional(CONF_STATE_CLOSING, default=3): cv.positive_int,
@@ -271,6 +271,12 @@ COVERS_SCHEMA = BASE_COMPONENT_SCHEMA.extend(
             CONF_STATUS_REGISTER_TYPE,
             default=CALL_TYPE_REGISTER_HOLDING,
         ): vol.In([CALL_TYPE_REGISTER_HOLDING, CALL_TYPE_REGISTER_INPUT]),
+        vol.Optional(CONF_VERIFY): vol.Maybe(
+            {
+                vol.Optional(CONF_ADDRESS): cv.positive_int,
+                vol.Optional(CONF_ADDRESS_CLOSE): cv.positive_int,
+            }
+        ),
     }
 )
 
