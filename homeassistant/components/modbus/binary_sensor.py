688dff5cab3e8249bf30679d7d8068d8304bd140
mike.toggweiler <mike.toggweiler@tegonal.com>
2021-10-04 23:27:11 +0200

    fixed group detection, added support for calculated positon based on time_to_max_seconds

--- homeassistant/components/modbus/binary_sensor.py
+++ homeassistant/components/modbus/binary_sensor.py
@@ -86,7 +86,7 @@ class ModbusBinarySensor(BasePlatform, RestoreEntity, BinarySensorEntity):
             slaveId,
             input_type,
             address,
-            result.bits,
+            result.bits[address],
         )        
         self._attr_is_on = result.bits[address] & 1
         self._attr_available = True
