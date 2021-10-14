37e72cbf3aba1d68c5ff006f3eb52e7bae268f22
mike.toggweiler <mike.toggweiler@tegonal.com>
2021-10-14 10:56:25 +0200

    added support for call_active flag

--- homeassistant/components/modbus/climate.py
+++ homeassistant/components/modbus/climate.py
@@ -132,6 +132,9 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
             for i in range(0, len(as_bytes), 2)
         ]
         registers = self._swap_registers(raw_regs)
+        if self._call_active:
+            return
+        self._call_active = True
 
         if self._data_type in (
             DataType.INT16,
@@ -150,6 +153,7 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
                 [int(float(i)) for i in registers],
                 CALL_TYPE_WRITE_REGISTERS,
             )
+        self._call_active = False
         self._attr_available = result is not None
         await self.async_update()
 
@@ -168,6 +172,7 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
             self._count,
             CALL_TYPE_REGISTER_HOLDING,
         )
+        self._call_active = False
 
         new_target_temperature = await self._async_read_register(result, 0)
         if new_target_temperature != self._attr_target_temperature:
@@ -188,7 +193,7 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
     ) -> None:
         """Update Target & Current Temperature."""
         if result is None:
-            self._available = False
+            self._attr_available = False
             return
         if input_type == CALL_TYPE_REGISTER_HOLDING:
             new_value = await self._async_read_register(result, address)
@@ -200,7 +205,7 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
             if new_value != self._attr_current_temperature:
                 self._attr_current_temperature = new_value
                 self.async_write_ha_state()
-        self._available = True
+        self._attr_available = True
 
     async def _async_read_register(
         self, result: ModbusResponse, address: int
